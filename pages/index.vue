d1937bcf4e37f63c758fa5144859f4e0cde3d6de
magalutfullaev <mhoja9494@gmail.com>
2024-08-01 18:26:38 +0500

    ✅ add: Footer

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,13 @@
+<script setup lang="ts">
+import Footer from "~/components/Footer.vue";
+</script>
+
 <template>
   <Header />
+
+  <Footer />
 </template>
+
+<style lang="scss" scoped>
+
+</style>
